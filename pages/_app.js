4e547aeb558a5afd0d7c23bc4589d3b47a328f73
Rehmet Yeshanew <ryfgmz87@gmail.com>
2023-03-27 22:47:06 -0400

    push app structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import 'tailwindcss/tailwind.css';
 import '@/styles/globals.scss'
+import React, {useEFfect, useState} from 'react'
+import {Layout} from '../components'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  
+  )
 }
