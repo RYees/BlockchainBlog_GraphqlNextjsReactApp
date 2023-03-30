eb4e42bd17739e9f20750105f8097abaf2382142
Rehmet Yeshanew <ryfgmz87@gmail.com>
2023-03-30 10:05:18 -0400

    comment componenet done

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -44,8 +44,8 @@ const PostDetail = ({ post }) => {
 
   return (
     <>
-    <div>data</div>
-      {/* <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
+    {/* <div>data</div> */}
+      <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
         <div className="relative overflow-hidden shadow-md mb-6">
           <img src={post.featuredImage.url} alt="" className="object-top h-full w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg" />
         </div>
@@ -75,7 +75,7 @@ const PostDetail = ({ post }) => {
             return getContentFragment(index, children, typeObj, typeObj.type);
           })}
         </div>
-      </div> */}
+      </div>
 
     </>
   );
